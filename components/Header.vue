4870e49cb1726c626088721b01b6c15ba78afd12
Gilbert <akhamgilbert@gmail.com>
2023-10-01 01:58:06 +0530

    changes in nuxt.config.ts

--- components/Header.vue
+++ components/Header.vue
@@ -1,8 +1,8 @@
 <template>
-    <NuxtLayout>
+   
     <div class="bg-[#000837] max-w-screen py-5">
         <div class="flex items-center justify-center mb-5 md:mb-0">
-            <NuxtImg class="md:w-32 w-16" src="/logoOne.svg" alt="logo"/>
+            <img class="md:w-32 w-16" src="/logoOne.svg" alt="logo"/>
             <h2 class="text-center text-2xl md:text-7xl font-bold text-white">
                 Quality
                 <span class="text-[#FFC03D]">Efforts</span>.com
@@ -40,7 +40,7 @@
         </div>
     </div>
     </div>
-</NuxtLayout>
+
 </template>
 
 <script >
